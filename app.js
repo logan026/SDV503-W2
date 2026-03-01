4c71d6c4e3bdf0d41e0c8325c98199e910572cbf
logan026 <logan-pike@live.nmit.ac.nz>
2026-03-02 11:18:39 +1300

    Added lottery system

--- app.js
+++ app.js
@@ -26,11 +26,26 @@ if (operator === "+") {
     X = A / B;
 }
 console.log("random time!!! your first random number is " + A + " your random operator is " + operator + " and your second random number is " + B + " and the result of the operation is " + X);
+function CheckLotto() {
+    A = Math.floor(Math.random() * 5) + 1;
+    console.log("Generated Lottery Number: " + A);
+    const lotteryNumber = document.getElementById("lottery").value;
+    if (isNaN(lotteryNumber) || lotteryNumber < 1 || lotteryNumber > 5) {
+        alert("Please enter a valid number between 1 and 5.");
+        return;
+    }
+    if (lotteryNumber === A.toString()) {
+        alert("Congratulations! You won the lottery!");
+    } else {
+        alert("Sorry, better luck next time.");
+            }
+    }
 
-var name = "alice";
-var age = 30;
-const PI = 3.14159;
+var name = "alice"; // Function - scoped, hoisted - avoid using in modern JavaScript
+let age = 30; //Block scoped, can be reasigned
+const PI = 3.14159; //Block scoped, cannot be reasigned
 let sckre = 0;
-score = 10;
-const max = 100;
-// max = 200; This will cause an error because max is a constant
+score = 10; // OK - reassignment allowed for let
+const max = 100; // max = 200; This will cause an error because max is a constant
+
+let address = "29 Woodstock Place";
