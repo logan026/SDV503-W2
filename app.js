b164852d202b7f12a82299f636a13b281810cadc
logan026 <logan-pike@live.nmit.ac.nz>
2026-03-02 10:17:25 +1300

    Created a js file named app and html

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+randomOperator = function() {
+    operator = Math.floor(Math.random() * 4 + 1);
+    if (operator === 1) {
+        return "+";
+    } else if (operator === 2) {
+        return "-";
+    } else if (operator === 3) {
+        return "*";
+    } else if (operator === 4) {
+        return "/";
+    } else {
+        return "error";
+    }
+}
+A = Math.floor(Math.random() * 100 + 1);
+B = Math.floor(Math.random() * 100 + 1);
+operator = randomOperator();
+if (operator === "+") {
+    X = A + B;
+} else if (operator === "-") {
+    X = A - B;
+} else if (operator === "*") {
+    X = A * B;
+} else if (operator === "/") {
+    X = A / B;
+}
+console.log("random time!!! your first random number is " + A + " your random operator is " + operator + " and your second random number is " + B + " and the result of the operation is " + X);
